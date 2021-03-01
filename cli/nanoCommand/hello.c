41cda08a78374ec3ec31ef09529bf50f25e8bbd5
GNUOverYooouu <69485115+DamieFC@users.noreply.github.com>
2021-03-01 10:45:16 -0800

    Update hello.c

--- cli/nanoCommand/hello.c
+++ cli/nanoCommand/hello.c
@@ -1,5 +1,9 @@
-#include "../lib/stdio.h"
+#include "../../lib/stdio.h"
+#include "../../lib/cprompt.h"
 
 int main(){
-	
+	prscnPrompt();
+	if(input == "hello" || "Hello"){
+		printf(Uh... Hi?);
+	}
 }
