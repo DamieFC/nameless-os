f2f18eb4223efabfd73c7ed160876904506b4abb
GNUOverYooouu <dtortoise99@gmail.com>
2021-03-26 09:45:22 -0700

    Update main.c

--- cli/nanoCommand/internals/main.c
+++ cli/nanoCommand/internals/main.c
@@ -1,7 +1,8 @@
 #include "../../../lib/stdio.h"
 #include "../../../drivers/video/vbe.h"
-
+#include "../../../lib/prompt.h"
 int main(){
     Color red = {245, 49, 0}, gray = {105, 100, 99}, blue = {19, 144, 194};
     VBE_display_circle(50, 50, 50);
+    printf("%s", prompt);
 }
